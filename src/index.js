3e7d73e7b94e768ab06051529f90bf7800a4f3ca
Kanetomo <52394458+Kanetomo@users.noreply.github.com>
2020-11-28 03:19:42 +0900

    完了ボタンのデリーと機能

--- src/index.js
+++ src/index.js
@@ -18,16 +18,16 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
   });
 
   //button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    // 押された削除本の親タグ(div)を未完了リストから削除
-    const deleteTaret = deleteButton.parentNode;
-    document.getElementById("incomplelte-list").removeChild(deleteTaret);
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
@@ -37,6 +37,11 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplelte-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplelte-list").removeChild(target);
 };
 
 document
